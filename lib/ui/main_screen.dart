ab80ba1a8be5a3ec8223f11a135015c05441bae3
angapkpro0123 <42203666+angapkpro0123@users.noreply.github.com>
2020-05-13 17:52:05 +0700

    Update main screen: Add event for user to pick date in date card

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -5,7 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import './dates_list.dart';
 
-int _lastFocusedIndex = 0; // biến để check xem Icon thứ mấy dc focus trc đó
+int _lastFocusedIconIndex = 0; // biến để check xem Icon thứ mấy dc focus trc đó
 String _dayName; // variables for displaying time in Calendar
 
 GlobalKey _bottomMenuKey = GlobalKey();
@@ -19,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   TabController _tabController;
   List<String> _weekDates = []; // List để lưu 7 ngày trong tuần đó để hiển thị
   List<Widget> _dateCardList =
-  []; // List chứa các widget hiển thị card ngày trên Calendar
+      []; // List chứa các widget hiển thị card ngày trên Calendar
   List<Widget> _dateNameList = []; // List chứa các widget để hiển thị
   List<String> _weekDateNames = [
     "S",
@@ -33,9 +33,11 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
   int _currentDateIndex; // Biến để chứa vị trí của ngày hiện tại trong tuần
   int _increaseClickedTime =
-  0; // Biến để check xem ng dùng đã chuyển qua bao nhiêu tuần
+      0; // Biến để check xem ng dùng đã chuyển qua bao nhiêu tuần
   int _decreaseClickedTime =
-  0; // Biến để check xem ng dùng đã chuyển qua bao nhiêu tuần
+      0; // Biến để check xem ng dùng đã chuyển qua bao nhiêu tuần
+  int _lastFocusDate =
+      0; // Biến để check xem giá trị ngày trc đó đang dc chọn là ngày nào
 
   DateTime _pickedDate;
 
@@ -59,6 +61,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
 
     // Gọi hàm để check thứ tự ngày hiện tãi trong tuần
     _currentDateIndex = _currentDateNum();
+    _lastFocusDate = int.parse(_weekDates[0]);
 
     ///
     /// Xét xem ngày hiện tại là thứ mấy
@@ -80,8 +83,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         _dateNameList.add(_weekDateName(_weekDateNames[i].toString()));
       }
     }
-
-    setState(() {});
   }
 
   @override
@@ -112,7 +113,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                       Container(
                         decoration: BoxDecoration(
                             borderRadius:
-                            BorderRadius.all(Radius.circular(30))),
+                                BorderRadius.all(Radius.circular(30))),
                         height: 250,
                         child: ClipRRect(
                           child: Image.asset(
@@ -177,7 +178,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                                 padding: const EdgeInsets.only(top: 20.0),
                                 child: Row(
                                   mainAxisAlignment:
-                                  MainAxisAlignment.spaceEvenly,
+                                      MainAxisAlignment.spaceEvenly,
                                   children: <Widget>[
                                     _dateNameList[0],
                                     _dateNameList[1],
@@ -194,15 +195,50 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                                     left: 20.0, right: 20.0, top: 15.0),
                                 child: Row(
                                   mainAxisAlignment:
-                                  MainAxisAlignment.spaceEvenly,
+                                      MainAxisAlignment.spaceEvenly,
                                   children: <Widget>[
-                                    _dateCardList[0],
-                                    _dateCardList[1],
-                                    _dateCardList[2],
-                                    _dateCardList[3],
-                                    _dateCardList[4],
-                                    _dateCardList[5],
-                                    _dateCardList[6],
+                                    InkWell(
+                                      child: _dateCardList[0],
+                                      onTap: () {
+                                        _changeFocusedCardColor(0);
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: _dateCardList[1],
+                                      onTap: () {
+                                        _changeFocusedCardColor(1);
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: _dateCardList[2],
+                                      onTap: () {
+                                        _changeFocusedCardColor(2);
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: _dateCardList[3],
+                                      onTap: () {
+                                        _changeFocusedCardColor(3);
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: _dateCardList[4],
+                                      onTap: () {
+                                        _changeFocusedCardColor(4);
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: _dateCardList[5],
+                                      onTap: () {
+                                        _changeFocusedCardColor(5);
+                                      },
+                                    ),
+                                    InkWell(
+                                      child: _dateCardList[6],
+                                      onTap: () {
+                                        _changeFocusedCardColor(6);
+                                      },
+                                    ),
                                   ],
                                 ),
                               ),
@@ -212,10 +248,13 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                             child: FlatButton(
                               onPressed: () {
                                 showDatePicker(
-                                    context: context,
-                                    initialDate: _pickedDate == null ? DateTime.now() : _pickedDate,
-                                    firstDate: DateTime(1900),
-                                    lastDate: DateTime(2200)).then((date) {
+                                        context: context,
+                                        initialDate: _pickedDate == null
+                                            ? DateTime.now()
+                                            : _pickedDate,
+                                        firstDate: DateTime(1900),
+                                        lastDate: DateTime(2200))
+                                    .then((date) {
                                   setState(() {
                                     _pickedDate = date;
                                   });
@@ -273,14 +312,14 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                     ),
                     Tab(
                         child: Icon(
-                          Icons.event,
-                          size: 30,
-                        )),
+                      Icons.event,
+                      size: 30,
+                    )),
                     Tab(
                         child: Icon(
-                          Icons.assistant_photo,
-                          size: 30,
-                        )),
+                      Icons.assistant_photo,
+                      size: 30,
+                    )),
                   ],
                 ),
               ),
@@ -314,7 +353,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       bottomNavigationBar: BubbleBottomBar(
         backgroundColor: Color(0xFFFAF3F0),
         opacity: .2,
-        currentIndex: _lastFocusedIndex,
+        currentIndex: _lastFocusedIconIndex,
         onTap: _changePage,
         borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
         elevation: 8,
@@ -331,7 +370,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               backgroundColor: Colors.redAccent,
               icon: Icon(CupertinoIcons.tags, size: 30, color: Colors.red),
               activeIcon:
-              Icon(CupertinoIcons.tags, size: 30, color: Colors.indigo),
+                  Icon(CupertinoIcons.tags, size: 30, color: Colors.indigo),
               title: Text(
                 "Recent",
                 style: TextStyle(color: Colors.red.shade900),
@@ -350,7 +389,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               backgroundColor: Colors.teal,
               icon: Icon(CupertinoIcons.book, size: 30, color: Colors.teal),
               activeIcon:
-              Icon(CupertinoIcons.book, size: 30, color: Colors.indigo),
+                  Icon(CupertinoIcons.book, size: 30, color: Colors.indigo),
               title: Text(
                 "Tasks List",
                 style: TextStyle(
@@ -361,7 +400,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               backgroundColor: Colors.green,
               icon: Icon(CupertinoIcons.settings, size: 30, color: Colors.cyan),
               activeIcon:
-              Icon(CupertinoIcons.settings, size: 30, color: Colors.indigo),
+                  Icon(CupertinoIcons.settings, size: 30, color: Colors.indigo),
               title: Text(
                 "Settings",
                 style: TextStyle(color: Colors.green.shade900),
@@ -374,7 +413,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   // Hàm để cập nhật vị trí tab hiện tại của bottom bar
   void _changePage(int value) {
     setState(() {
-      _lastFocusedIndex = value;
+      _lastFocusedIconIndex = value;
     });
   }
 
@@ -388,7 +427,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   }
 
   // Widget để hiển thị số trên Calendar
-  Widget _dateCard(String dateNum, double opacity) {
+  Widget _dateCard(String dateNum, double opacity, [Color color]) {
     return Container(
       width: 50,
       height: 70,
@@ -398,7 +437,8 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       child: Center(
         child: Text(
           "$dateNum",
-          style: GoogleFonts.roboto(fontSize: 17.0, color: Colors.white),
+          style: GoogleFonts.roboto(
+              fontSize: 17.0, color: color == null ? Colors.white : color),
         ),
       ),
     );
@@ -437,16 +477,17 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
             .add(new Duration(days: i + 7 * _increaseClickedTime))));
       }
 
+      // Add hiển thị của card ra screen
       for (int i = 0; i < 7; i++) {
         double opacity = 0;
-        if (_increaseClickedTime == 0) {
-          // Nếu đang ở tuần hiện tại
-          opacity = i == 0 ? 0.85 : 0.3;
-          _dateCardList.add(_dateCard(_weekDates[i].toString(), opacity));
-        } else {
-          opacity = 0.3;
-          _dateCardList.add(_dateCard(_weekDates[i].toString(), opacity));
-        }
+        opacity = _lastFocusDate == int.parse(_weekDates[i]) ? 0.85 : 0.3; // Nếu ngày đang focus có trong tuần đang xem
+        _dateCardList
+            .add(_dateCard(_weekDates[i].toString(), opacity, Colors.white));
+      }
+      if (_lastFocusDate != int.parse(_weekDates[0])) {
+        if (_decreaseClickedTime == 0)
+          _dateCardList[0] =
+              _dateCard(_weekDates[0], 0.3, Colors.yellow.shade500);
       }
     });
   }
@@ -467,15 +508,61 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
             .add(new Duration(days: i - 7 * _decreaseClickedTime))));
       }
 
+      // Add hiển thị của card ra screen
       for (int i = 0; i < 7; i++) {
         double opacity = 0;
-        if (_increaseClickedTime == 0) {
-          // Nếu đang ở tuần hiện tại
-          opacity = i == 0 ? 0.85 : 0.3;
-          _dateCardList.add(_dateCard(_weekDates[i].toString(), opacity));
+        opacity = _lastFocusDate == int.parse(_weekDates[i]) ? 0.85 : 0.3; // Nếu ngày đang focus có trong tuần đang xem
+        _dateCardList
+            .add(_dateCard(_weekDates[i].toString(), opacity, Colors.white));
+      }
+      if (_lastFocusDate != int.parse(_weekDates[0])) {
+        if (_decreaseClickedTime == 0)
+          _dateCardList[0] =
+              _dateCard(_weekDates[0], 0.3, Colors.yellow.shade500);
+      }
+    });
+  }
+
+  // Hàm sự kiện để thay đổi màu thẻ khi thẻ được focus
+  void _changeFocusedCardColor(int selectedIndex) {
+    setState(() {
+      // Nếu ng dùng đang dừng ở tuần khác hiện tại
+      if (_increaseClickedTime != 0 && _decreaseClickedTime != 0) {
+        if (int.parse(_weekDates[selectedIndex]) != _lastFocusDate) {
+          _dateCardList[selectedIndex] =
+              _dateCard(_weekDates[selectedIndex], 0.85);
+          _lastFocusDate = int.parse(_weekDates[selectedIndex]);
+
+          for (int i = 0; i < 7; i++) {
+            if (selectedIndex != i) {
+              _dateCardList[i] = _dateCard(_weekDates[i], 0.3);
+            }
+          }
+        }
+      } else if (_increaseClickedTime == 0 && _decreaseClickedTime == 0) {
+        // Nếu ng dùng đang dừng ở tuần hiện tại
+        if (selectedIndex == 0) {
+          _dateCardList[0] = _dateCard(_weekDates[0].toString(), 0.85);
+          _lastFocusDate = int.parse(_weekDates[selectedIndex]);
+          for (int i = 1; i < 7; i++) {
+            if (selectedIndex != i) {
+              _dateCardList[i] = _dateCard(_weekDates[i], 0.3);
+            }
+          }
         } else {
-          opacity = 0.3;
-          _dateCardList.add(_dateCard(_weekDates[i].toString(), opacity));
+          if (int.parse(_weekDates[selectedIndex]) != _lastFocusDate) {
+            _dateCardList[0] = _dateCard(
+                _weekDates[0].toString(), 0.3, Colors.yellow.shade500);
+            _dateCardList[selectedIndex] =
+                _dateCard(_weekDates[selectedIndex], 0.85);
+            _lastFocusDate = int.parse(_weekDates[selectedIndex]);
+
+            for (int i = 1; i < 7; i++) {
+              if (selectedIndex != i) {
+                _dateCardList[i] = _dateCard(_weekDates[i], 0.3);
+              }
+            }
+          }
         }
       }
     });

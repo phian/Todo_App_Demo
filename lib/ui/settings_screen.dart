201cbee1b0a6c551f4fa99e98da9543f6b01eec1
angapkpro0123 <42203666+angapkpro0123@users.noreply.github.com>
2020-05-17 10:55:28 +0700

    Update settings screen: Reduce code and add temp event when user click on the menu widget

--- lib/ui/settings_screen.dart
+++ lib/ui/settings_screen.dart
@@ -7,11 +7,33 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  List<IconData> _settingMenuIcons = [
+    Icons.brightness_7,
+    Icons.search,
+    Icons.help_outline,
+    Icons.info_outline,
+    Icons.explore,
+    Icons.account_circle
+  ];
+  List<String> _settingMenuTexts = [
+    "Preference",
+    "Search",
+    "Help",
+    "About",
+    "Getting Started",
+    "Account"
+  ];
+  List<Widget> _settingMenuWidgets = [];
+
   @override
   Widget build(BuildContext context) {
+    _initSettingMenuWidget(); // Gọi hàm để khởi tạo
+
     return Container(
       color: Color(0xFFFAF3F0),
-      padding: EdgeInsets.only(top: MediaQuery.of(context).size.width - 120.0, left: MediaQuery.of(context).size.width / 2 - 120.0),
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.width - 120.0,
+          left: MediaQuery.of(context).size.width / 2 - 120.0),
       child: Column(
         children: <Widget>[
           Container(
@@ -27,101 +49,152 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
           InkWell(
             onTap: () {
-              print("Preference clicked");
+              _changeFocusMenuWidgetColor(0, false);
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "Preference",
-                style: GoogleFonts.abhayaLibre(
-                  fontSize: 20.0
-                ),
-              ),
-            ),
+            onTapCancel: () {
+              _changeFocusMenuWidgetColor(0, false);
+            },
+            onHighlightChanged: (isChanged) {
+              if(isChanged) {
+                _changeFocusMenuWidgetColor(0, true);
+              }
+            },
+            child: _settingMenuWidgets[0],
           ),
           SizedBox(
             height: 10.0,
           ),
           InkWell(
             onTap: () {
-              print("Search clicked");
+              _changeFocusMenuWidgetColor(1, false);
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "Search",
-                style: GoogleFonts.abhayaLibre(
-                  fontSize: 20.0
-                ),
-              ),
-            ),
+            onTapCancel: () {
+              _changeFocusMenuWidgetColor(1, false);
+            },
+            onHighlightChanged: (isChanged) {
+              if(isChanged) {
+                _changeFocusMenuWidgetColor(1, true);
+              }
+            },
+            child: _settingMenuWidgets[1],
           ),
           SizedBox(
             height: 10.0,
           ),
           InkWell(
             onTap: () {
-              print("Help clicked");
+              _changeFocusMenuWidgetColor(2, false);
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "Help",
-                style: GoogleFonts.abhayaLibre(
-                  fontSize: 20.0
-                ),
-              ),
-            ),
+            onTapCancel: () {
+              _changeFocusMenuWidgetColor(2, false);
+            },
+            onHighlightChanged: (isChanged) {
+              if(isChanged) {
+                _changeFocusMenuWidgetColor(2, true);
+              }
+            },
+            child: _settingMenuWidgets[2],
           ),
           SizedBox(
             height: 10.0,
           ),
           InkWell(
             onTap: () {
-              print("About clicked");
+              _changeFocusMenuWidgetColor(3, false);
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "About",
-                style: GoogleFonts.abhayaLibre(
-                  fontSize: 20.0
-                ),
-              ),
-            ),
+            onTapCancel: () {
+              _changeFocusMenuWidgetColor(3, false);
+            },
+            onHighlightChanged: (isChanged) {
+              if(isChanged) {
+                _changeFocusMenuWidgetColor(3, true);
+              }
+            },
+            child: _settingMenuWidgets[3],
           ),
           SizedBox(
             height: 10.0,
           ),
           InkWell(
             onTap: () {
-              print("Getting started clicked");
+              _changeFocusMenuWidgetColor(4, false);
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "Getting started",
-                style: GoogleFonts.abhayaLibre(
-                  fontSize: 20.0
-                ),
-              ),
-            ),
+            onTapCancel: () {
+              _changeFocusMenuWidgetColor(4, false);
+            },
+            onHighlightChanged: (isChanged) {
+              if(isChanged) {
+                _changeFocusMenuWidgetColor(4, true);
+              }
+            },
+            child: _settingMenuWidgets[4],
           ),
           SizedBox(
             height: 10.0,
           ),
           InkWell(
             onTap: () {
-              print("Account clicked");
+              _changeFocusMenuWidgetColor(5, false);
             },
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "Account",
-                style: GoogleFonts.abhayaLibre(
-                  fontSize: 20.0
-                ),
-              ),
+            onTapCancel: () {
+              _changeFocusMenuWidgetColor(5, false);
+            },
+            onHighlightChanged: (isChanged) {
+              if(isChanged) {
+                _changeFocusMenuWidgetColor(5, true);
+              }
+            },
+            child: _settingMenuWidgets[5],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Hàm để khởi tạo các widgets cho setting menu
+  void _initSettingMenuWidget() {
+    for (int i = 0; i < 6; i++) {
+      _settingMenuWidgets.add(_settingsMenuWidget(_settingMenuIcons[i],
+          _settingMenuTexts[i], Colors.black, Colors.black));
+    }
+  }
+
+  // Hàm để reset màu của icon và text của menu widget dc chọn
+  void _changeFocusMenuWidgetColor(int focusedIndex, bool isFocused) {
+    if (isFocused)
+      setState(() {
+        _settingMenuWidgets[focusedIndex] = _settingsMenuWidget(
+            _settingMenuIcons[focusedIndex],
+            _settingMenuTexts[focusedIndex],
+            Colors.grey,
+            Colors.grey);
+      });
+    else
+      setState(() {
+        _settingMenuWidgets[focusedIndex] = _settingsMenuWidget(
+            _settingMenuIcons[focusedIndex],
+            _settingMenuTexts[focusedIndex],
+            Colors.black,
+            Colors.black);
+      });
+  }
+
+  // Widget để hiển thị trong setting menu
+  Widget _settingsMenuWidget(
+      IconData icon, String menuText, Color iconColor, Color textColor) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: <Widget>[
+          Icon(
+            icon,
+            color: iconColor,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0),
+            child: Text(
+              "$menuText",
+              style: GoogleFonts.abhayaLibre(fontSize: 20.0, color: textColor),
             ),
           ),
         ],
